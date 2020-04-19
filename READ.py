5d6210e5759017dfae4c22e55997d7b4a6c0b74a
Mike Kao <skybow@gmail.com>
2020-04-20 01:17:58 +0800

    add avgm

--- READ.py
+++ READ.py
@@ -1,5 +1,5 @@
 data = [] # 建立名為 data 的空清單
-counter = 0
+counter = 0 # 新增計數用變數
 with open('reviews.txt', 'r') as f: # r = read , as f = 當作 f
 	for line in f: # 將 f 每筆資料命名為變數 line
 		data.append(line) #line (每筆資料) append 進 data 裡面
@@ -10,3 +10,10 @@ with open('reviews.txt', 'r') as f: # r = read , as f = 當作 f
 # print(len(data)) # 利用 len 列出總筆數
 # print(data) # 會將所有100萬筆資料全部印出
 # print(data[0]) # 將第1筆資料印出
+print('總共有', len(data), '筆資料') # 需注意逗點
+
+sum_len = 0 # 新增每筆留言長度變數
+for comment in data: # 將 data 每筆資料命名為變數 comment
+	sum_len = sum_len +len(comment)
+	# print(sum_len) # 顯示加總過程
+print('留言平均長度為', sum_len/len(data))
